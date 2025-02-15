5c8daf3b9477bcf4841f578ea42e077e4ea78293
Harg <ventzislav.shabansky@gmail.com>
2025-02-15 11:46:52 +0200

    Optimize guess function

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,31 @@ fn main() {
     let guess1: [i8; 4] = [1, 2, 3, 4];
     let guess2: [i8; 4] = [4, 3, 2, 1];
     let guess3: [i8; 4] = [1, 2, 3, 5];
-    guess(guess1);
-    guess(guess2);
-    guess(guess3);
+
+    debug_assert_eq!(guess(guess1), (4, 0));
+    debug_assert_eq!(guess(guess2), (0, 4));
+    debug_assert_eq!(guess(guess3), (3, 0));
 }
 
-fn guess(guess: [i8; 4]) {
+fn guess(guess: [i8; 4]) -> (i8, i8) {
     let number: [i8; 4] = [1, 2, 3, 4];
-    
-    let mut cows:i8 = 0;
-    let mut bulls:i8 = 0;
 
-    let mut i:usize = 0;
-    loop {
-        if i == 4 {
-            break;
+    let mut cows: i8 = 0;
+    let mut bulls: i8 = 0;
+
+    for (guess_index, &guess_value) in guess.iter().enumerate() {
+        if guess_value == number[guess_index] {
+            bulls += 1;
+            continue;
         }
 
-        if guess[i] == number[i] {
-            bulls += 1
-        } else {
-            let mut j:usize = 0;
-            loop {
-                if j == 4 {
-                    break;
-                }
-
-                if guess[i] == number[j] {
-                    cows += 1;
-                }
-
-                j += 1;
+        for (_, &num_value) in number.iter().enumerate() {
+            if guess_value == num_value {
+                cows += 1;
+                continue;
             }
         }
-        i += 1;
     }
-    println!("Cows: {}, Bulls: {}", cows, bulls);
+
+    (bulls, cows)
 }
