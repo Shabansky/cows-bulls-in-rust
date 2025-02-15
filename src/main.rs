040117c7f395ab9c4d5469088cf2a8c8f4bd8897
Harg <ventzislav.shabansky@gmail.com>
2025-02-15 12:34:08 +0200

    Add simple set guess function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,22 @@ fn main() {
     debug_assert_eq!(guess(guess1), (4, 0));
     debug_assert_eq!(guess(guess2), (0, 4));
     debug_assert_eq!(guess(guess3), (3, 0));
+
+    debug_assert_eq!(set_guess("1234"), [1, 2, 3, 4]);
+    debug_assert_eq!(set_guess("abcd"), [0, 0, 0, 0]);
+}
+
+fn set_guess(guess: &str) -> [i8; 4] {
+    let mut guess_arr = [0; 4];
+    for (index, character) in guess.chars().enumerate() {
+        if !character.is_numeric() {
+            println!("{character} is not a numeral");
+            break;
+        }
+
+        guess_arr[index] = character as i8;
+    }
+    guess_arr
 }
 
 fn guess(guess: [i8; 4]) -> (i8, i8) {
@@ -14,8 +30,8 @@ fn guess(guess: [i8; 4]) -> (i8, i8) {
     let mut cows: i8 = 0;
     let mut bulls: i8 = 0;
 
-    for (guess_index, &guess_value) in guess.iter().enumerate() {
-        if guess_value == number[guess_index] {
+    for (guess_column, &guess_value) in guess.iter().enumerate() {
+        if guess_value == number[guess_column] {
             bulls += 1;
             continue;
         }
