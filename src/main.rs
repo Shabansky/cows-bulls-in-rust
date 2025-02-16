98db7827eac2f8f0ade47f13078256181dcc206a
Harg <ventzislav.shabansky@gmail.com>
2025-02-16 10:59:06 +0200

    Link to stdin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::io;
 fn main() {
+    asserts();
+
+    let mut input = String::new();
+
+    let _ = io::stdin().read_line(&mut input);
+
+    let guess_input = set_guess(&input.trim());
+
+    match guess_input {
+        Some(_) => {
+            println!("{:?}", guess(guess_input.unwrap()))
+        }
+        None => println!("{} is not 4 digit or a numeral", input.trim()),
+    }
+}
+
+fn asserts() {
     let guess1: [i8; 4] = [1, 2, 3, 4];
     let guess2: [i8; 4] = [4, 3, 2, 1];
     let guess3: [i8; 4] = [1, 2, 3, 5];
@@ -9,9 +27,14 @@ fn main() {
 
     debug_assert_eq!(set_guess("1234"), Some([1, 2, 3, 4]));
     debug_assert_eq!(set_guess("abcd"), None);
+    debug_assert_eq!(set_guess("1234   "), None);
 }
 
 fn set_guess(guess: &str) -> Option<[i8; 4]> {
+    if guess.len() != 4 {
+        return None;
+    }
+
     let mut guess_arr = [0; 4];
     for (index, character) in guess.chars().enumerate() {
         if !character.is_numeric() {
