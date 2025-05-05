2d3284d391f49d276683fd4213af1118742dda05
Ventzislav Shabansky <ventzislav.shabansky@gmail.com>
2025-05-05 18:55:15 +0300

    Add unit tests for game creation and player add

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -12,7 +12,7 @@ pub struct Game {
     pub players: Vec<Player>,
     is_over: bool,
     current_player: usize,
-    winning_player: Option<Player>,
+    winning_player: usize,
 }
 
 impl Game {
@@ -21,7 +21,7 @@ impl Game {
             players: vec![],
             is_over: false,
             current_player: 0,
-            winning_player: None,
+            winning_player: 0,
         }
     }
 
@@ -45,7 +45,6 @@ impl Game {
 
             let guess = guess::Guess::new(number.unwrap());
         }
-
     }
 
     pub fn guess(&self, guess: Number, player: &Player) {
@@ -55,5 +54,31 @@ impl Game {
         guess.process_against(number);
 
         //TODO: What should I do with this guess?
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn new_game_has_correct_starting_settings() {
+        let new_game = Game::new();
+
+        assert_eq!(0, new_game.players.iter().count());
+        assert_eq!(false, new_game.is_over);
+        assert_eq!(0, new_game.current_player);
+        assert_eq!(0, new_game.winning_player);
+    }
+
+    #[test]
+    fn add_player_updates_game_players_count() {
+        let mut new_game = Game::new();
+
+        assert_eq!(0, new_game.players.iter().count());
+        let player = Player::new(String::from("Player"), Number::new([1, 2, 3, 4]));
+        new_game.add_player(player);
+
+        assert_eq!(1, new_game.players.iter().count());
     }
 }
